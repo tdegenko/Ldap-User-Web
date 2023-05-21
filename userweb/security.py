b8bb9731609382c3ab0553e3771d4e889d6598ba
tanner <tanner@Book.mithri.date>
2023-05-21 19:32:11 -0400

    Fixes issue with message display, and issue resulting from removing logged in users.

--- userweb/security.py
+++ userweb/security.py
@@ -1,9 +1,16 @@
 import usermanagement
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def groupfinder(userid, request):
     conn = request.registry.settings["ldap.server"].connect()
-    groups = conn.User(userid).get_groups()
-    groups = [g.cn for g in [groups["primary"]] + groups["secondary"]]
+    try:
+        groups = conn.User(userid).get_groups()
+        groups = [g.cn for g in [groups["primary"]] + groups["secondary"]]
+    except Exception as e:
+        log.exception(e)
+        return []
 
     return groups
